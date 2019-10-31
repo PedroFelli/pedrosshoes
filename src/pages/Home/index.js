8790d819a72cc11483f42191405b286eac3c0b9a
PedroFelli <pedrofellipeunai@gmail.com>
2019-10-31 10:34:50 -0300

    carrinho de compras e home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
+          alt=""
+        />
+        <strong>Nike air</strong>
+        <span>R$200</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicinonar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
+          alt=""
+        />
+        <strong>Nike air</strong>
+        <span>R$200</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicinonar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
+          alt=""
+        />
+        <strong>Nike air</strong>
+        <span>R$200</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicinonar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
+          alt=""
+        />
+        <strong>Nike air</strong>
+        <span>R$200</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicinonar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
+          alt=""
+        />
+        <strong>Nike air</strong>
+        <span>R$200</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicinonar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
+          alt=""
+        />
+        <strong>Nike air</strong>
+        <span>R$200</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicinonar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
+          alt=""
+        />
+        <strong>Nike air</strong>
+        <span>R$200</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />3
+          </div>
+          <span>Adicinonar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
