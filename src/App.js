59f11374937d5a3366d349c26af9b49eaa0bc062
PedroFelli <pedrofellipeunai@gmail.com>
2019-10-31 22:14:05 -0300

    adicionando o redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Routes from './router';
+import { Provider } from 'react-redux';
 
+import Routes from './router';
 import GlobalStyke from './styles/global';
 import Header from './components/Header';
 
+import store from './store';
+
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyke />
-      <Header />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyke />
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,109 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
-          alt=""
-        />
-        <strong>Nike air</strong>
-        <span>R$200</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>Adicinonar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
-          alt=""
-        />
-        <strong>Nike air</strong>
-        <span>R$200</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>Adicinonar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
-          alt=""
-        />
-        <strong>Nike air</strong>
-        <span>R$200</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>Adicinonar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
-          alt=""
-        />
-        <strong>Nike air</strong>
-        <span>R$200</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>Adicinonar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
-          alt=""
-        />
-        <strong>Nike air</strong>
-        <span>R$200</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>Adicinonar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
-          alt=""
-        />
-        <strong>Nike air</strong>
-        <span>R$200</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>Adicinonar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-command-masculino/18/HZM-0354-118/HZM-0354-118_detalhe2.jpg?ims=326x"
-          alt=""
-        />
-        <strong>Nike air</strong>
-        <span>R$200</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-          <span>Adicinonar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" />3
+              </div>
+              <span>Adicinonar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
